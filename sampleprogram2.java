2c667a124b95c8769a08bb7130fdea69b7d3242a
vandana <harivandana.jampani@gmail.com>
2023-09-03 16:31:18 +0530

    s2

---
+++ sampleprogram2.java
@@ -0,0 +1,12 @@
+/* 5 color names*/
+class Colors
+{
+    public static void main (String args[])
+    {
+         System.out.println("blue");
+         System.out.println("white");
+         System.out.println("red");
+         System.out.println("pink");
+         System.out.println("green");
+     }
+}
