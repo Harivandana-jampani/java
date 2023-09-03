f6578fc82b8239aa55e1e11ff1e59573f1fe8634
vandana <harivandana.jampani@gmail.com>
2023-09-03 16:35:19 +0530

    s4

---
+++ sampleprogram4.java
@@ -0,0 +1,12 @@
+//5 fruit names//
+class Fruitname
+{
+    public static void main (String args[])
+    {
+         System.out.println("Mango");
+         System.out.println("apple");
+         System.out.println("orange");
+         System.out.println("banana");
+         System.out.println("watermelon");
+     }
+}
