9b60bedf439e66c6c300af7471b113477e533425
vandana <harivandana.jampani@gmail.com>
2023-09-03 16:40:30 +0530

    s5

---
+++ sampleprogram5.java
@@ -0,0 +1,12 @@
+//5 subjects//
+class Subjects
+{
+    public static void main (String args[])
+    {
+         System.out.println("hindi");
+         System.out.println("physics");
+         System.out.println("ECA");
+         System.out.println("ANN");
+         System.out.println("EDC");
+     }
+}
