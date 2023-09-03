c5309cc9e65dd3398607d17484c47007051de892
vandana <harivandana.jampani@gmail.com>
2023-09-03 16:28:56 +0530

    s1

---
+++ sampleprogram1.java
@@ -0,0 +1,12 @@
+/* 5 bestfriends names*/
+class Bestfriends
+{
+    public static void main (String args[])
+    {
+         System.out.println("yamini");
+         System.out.println("swapna");
+         System.out.println("kavya");
+         System.out.println("naveen");
+         System.out.println("vishnu");
+     }
+}
