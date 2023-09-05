f368d985c4e0a0d8f942085d95fe76263c943ecb
vandana <harivandana.jampani@gmail.com>
2023-09-05 07:30:00 +0530

    d2

---
+++ datatypes2.java
@@ -0,0 +1,12 @@
+//Bytedatatype
+class ByteDataTypeDemo
+{
+    public static void main (String args[])
+    {
+      byte a=10;
+      System.out.println(a);
+      a=20;
+      System.out.println(a);
+      
+     }
+}
