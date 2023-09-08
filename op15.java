1ed38f1782157226e5d320158dceaa776ddeb7be
vandana <harivandana.jampani@gmail.com>
2023-09-08 10:08:24 +0530

    op15

---
+++ op15.java
@@ -0,0 +1,11 @@
+//LesserthaneualDemo
+class LesserthaneualDemo
+{
+    public static void main (String args[])
+    {
+     int a=10;
+     int b=20;
+     boolean c=(a<=b);
+     System.out.println(c);
+     }
+}
