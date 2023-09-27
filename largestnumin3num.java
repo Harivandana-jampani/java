fe2d110f0271d4d6e32e98a5fe565fdd779976b3
Harivandana-jampani <harivandana.jampani>
2023-09-27 10:08:03 +0530

    grester num amoung 3 num

---
+++ largestnumin3num.java
@@ -0,0 +1,23 @@
+//largest among 3 numbers
+package com.gentech.Demo27;
+
+public class Evenorodddemo {
+
+	public static void main(String[] args) {
+	int x=11;
+	int y=10;
+	int z=12;
+	if((x>=y)&&(x>=z))
+	{
+		System.out.println("x is greater");
+	}
+	else if((y>=z)&&(y>=x))
+	{
+		System.out.println("y is greater");
+	}
+	else
+	{
+		System.out.println("z is greater");
+	}
+	}
+
