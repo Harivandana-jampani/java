bf7f126a2ccc5a37e3414a9b7a6d3507ae04ea59
Harivandana-jampani <harivandana.jampani>
2023-09-27 10:20:16 +0530

    month name

---
+++ months.java
@@ -0,0 +1,77 @@
+//month name 
+package com.gentech.Demo27;
+
+public class Evenorodddemo {
+
+	public static void main(String[] args) {
+		int month=10;
+		switch (month)
+		{
+		case 1:
+		{
+			System.out.println("january");
+			break;
+		}
+		case 2:
+		{
+			System.out.println("febuary");
+			break;
+		}
+		case 3:
+		{
+			System.out.println("march");
+			break;
+		}
+		case 4:
+		{
+			System.out.println("april");
+			break;
+		}
+		case 5:
+		{
+			System.out.println("june");
+			break;
+		}
+		case 6:
+		{
+			System.out.println("july");
+			break;
+		}
+		case 7:
+		{
+			System.out.println("may");
+			break;
+		}
+		case 8:
+		{
+			System.out.println("august");
+			break;
+		}
+		case 9:
+		{
+			System.out.println("september");
+			break;
+		}
+		case 10:
+		{
+			System.out.println("october");
+			break;
+		}
+		case 11:
+		{
+			System.out.println("november");
+			break;
+		}
+
+	    case 12:
+	    {
+		System.out.println("december");
+		break;
+	    }
+	    default:
+	    	System.out.println("invalid month");	
+		}
+			
+	}
+
+}
